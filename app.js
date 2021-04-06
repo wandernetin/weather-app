7626272aa239b48ed4cb771c640c86e17345be14
Wander Neto <wander.dmneto@gmail.com>
2021-04-06 21:26:28 +1000

    including yargs

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
+const yargs = require('yargs');
+
 const forecast = require('./utils/forecast');
-const geocode = require('./utils/geocode')
+const geocode = require('./utils/geocode');
 
-geocode('Brisbane', (error, data) => {
-    if (error)
-        return console.log(error);
+var city = yargs.argv.city;
 
-    forecast(data.center[1], data.center[0], (errorForecast, dataForecast) => {
-        if (errorForecast)
-            return console.log(errorForecast);
-
-        console.log(dataForecast.weather_descriptions[0] + '.');
-        console.log('It is currently ' + dataForecast.temperature + ' degrees out.');
-        console.log('It feel likes ' + dataForecast.feelslike + ' degrees.');
-        console.log('There is a ' + dataForecast.precip + '% chance of rain.');
-
+if(city) {
+    geocode(city, (error, data) => {
+        if (error)
+            return console.log(error);
+    
+        forecast(data.center[1], data.center[0], (errorForecast, dataForecast) => {
+            if (errorForecast)
+                return console.log(errorForecast);
+    
+            console.log(data.place_name);
+            console.log(dataForecast.weather_descriptions[0] + '.');
+            console.log('It is currently ' + dataForecast.temperature + ' degrees out.');
+            console.log('It feel likes ' + dataForecast.feelslike + ' degrees.');
+            console.log('There is a ' + dataForecast.precip + '% chance of rain.');
+    
+        })
     })
-})
+} else {
+    console.log('No city was selected!');
+}
+
 
--- utils/forecast.js
+++ utils/forecast.js
@@ -3,7 +3,7 @@ const request = require('request');
 const weatherStackToken = '60266219577ed2341baa752243552833';
 
 const forecast = (lat, long, callback) => {
-    const weatherUrl = 'http://api.weathe1rstack.com/current?access_key=' + weatherStackToken + '&query=' + lat + ',' + long + '&units=m';
+    const weatherUrl = 'http://api.weatherstack.com/current?access_key=' + weatherStackToken + '&query=' + lat + ',' + long + '&units=m';
     request({ url: weatherUrl, json: true }, (error, response) => {
         if (!error) {
             if (!response.body.error) {
