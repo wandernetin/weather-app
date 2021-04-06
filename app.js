6b2529eb7f505d2999f6379713d3c6b7ba28e4db
Wander Neto <wander.dmneto@gmail.com>
2021-04-06 21:14:06 +1000

    fixing issue

--- app.js
+++ app.js
@@ -2,19 +2,18 @@ const forecast = require('./utils/forecast');
 const geocode = require('./utils/geocode')
 
 geocode('Brisbane', (error, data) => {
-    if (error) {
-        console.log(error);
-    } else {
-        forecast(data.center[1], data.center[0], (errorForecast, dataForecast) => {
-            if (errorForecast) {
-                console.log(error);
-            } else {
-                console.log(dataForecast.weather_descriptions[0] + '.');
-                console.log('It is currently ' + dataForecast.temperature + ' degrees out.');
-                console.log('It feel likes ' + dataForecast.feelslike + ' degrees.');
-                console.log('There is a ' + dataForecast.precip + '% chance of rain.');
-            }
-        })
-    }
+    if (error)
+        return console.log(error);
+
+    forecast(data.center[1], data.center[0], (errorForecast, dataForecast) => {
+        if (errorForecast)
+            return console.log(errorForecast);
+
+        console.log(dataForecast.weather_descriptions[0] + '.');
+        console.log('It is currently ' + dataForecast.temperature + ' degrees out.');
+        console.log('It feel likes ' + dataForecast.feelslike + ' degrees.');
+        console.log('There is a ' + dataForecast.precip + '% chance of rain.');
+
+    })
 })
 
